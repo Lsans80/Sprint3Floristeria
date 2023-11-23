eac7baa14979af6721af22bdc46e851b9507717c
Lsans80 <luisa.sans@gmail.com>
2023-11-23 11:36:46 +0100

    Commit again due to an error

---
+++ src/main/java/org/example/n1Exe1/persistencia/BaseDeDatos.java
@@ -0,0 +1,57 @@
+package org.example.n1Exe1.persistencia;
+
+import org.example.n1Exe1.entidad.Producto;
+import org.example.n1Exe1.entidad.Ticket;
+
+import java.util.HashMap;
+
+public class BaseDeDatos {
+    HashMap<Integer, Producto> stock;
+    HashMap<Integer, Ticket> tickets;
+
+    public BaseDeDatos () {
+        stock = new HashMap<Integer, Producto>();
+        tickets = new HashMap<Integer, Ticket>();
+    }
+    public HashMap<Integer, Producto> getStock() {
+        return stock;
+    }
+    public HashMap<Integer, Ticket> getTickets() {
+        return tickets;
+    }
+    public void agregarProducto(Producto producto) {
+        stock.compute(producto.getProductoID(), (id, existingProducto) -> {
+            if (existingProducto != null) {
+                producto.setProductoCantidad(producto.getProductoCantidad() + existingProducto.getProductoCantidad());
+                return producto;
+            } else {
+                return producto;
+            }
+        });
+    }
+    public void agregarTicket(Ticket ticket) {
+        tickets.put(ticket.getTicketID(), ticket);
+    }
+    public Producto leerProducto(int id) {
+        return stock.get(id);
+    }
+    public Ticket leerTicket(int id) {
+        return tickets.get(id);
+    }
+    public void eliminarProducto(int id) {
+        stock.remove(id);
+    }
+    public void eliminarTicket(int id) {
+        tickets.remove(id);
+    }
+
+    //TODO Double?
+    public float getValorTotalStock() {
+        return (float) stock.values().stream().mapToDouble(producto -> producto.getProductoPrecio() * producto.getProductoCantidad()).sum();
+    }
+    public float getValorTotalTickets() {
+        return (float) tickets.values().stream().mapToDouble(Ticket::getTicketTotal).sum();
+
+    }
+
+}
