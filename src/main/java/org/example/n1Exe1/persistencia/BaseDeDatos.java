d8666be3230076333b56262076c55b1e30485cdd
Manel <msoler92@gmail.com>
2023-11-28 01:56:09 +0100

    Añadir save y load a BaseDeDatos

--- src/main/java/org/example/n1Exe1/persistencia/BaseDeDatos.java
+++ src/main/java/org/example/n1Exe1/persistencia/BaseDeDatos.java
@@ -3,6 +3,7 @@ package org.example.n1Exe1.persistencia;
 import org.example.n1Exe1.entidad.Producto;
 import org.example.n1Exe1.entidad.Ticket;
 
+import java.io.*;
 import java.util.HashMap;
 
 public class BaseDeDatos {
@@ -14,6 +15,7 @@ public class BaseDeDatos {
     private BaseDeDatos () {
         stock = new HashMap<Integer, Producto>();
         tickets = new HashMap<Integer, Ticket>();
+        load();
     }
 
     public BaseDeDatos instanciar() {
@@ -22,10 +24,10 @@ public class BaseDeDatos {
         }
         return instancia;
     }
-    public HashMap<Integer, Producto> getStock() {
+    public HashMap<Integer, Producto> listarProductos() {
         return stock;
     }
-    public HashMap<Integer, Ticket> getTickets() {
+    public HashMap<Integer, Ticket> listarTickets() {
         return tickets;
     }
     public void agregarProducto(Producto producto) {
@@ -61,5 +63,30 @@ public class BaseDeDatos {
         return (float) tickets.values().stream().mapToDouble(Ticket::getTicketTotal).sum();
     }
 
+    public void load() {
+        File database = new File ("database.txt");
+        if (database.exists()) {
+            try (FileInputStream fis = new FileInputStream(database);
+                 ObjectInputStream ois = new ObjectInputStream(fis)) {
+                stock = (HashMap<Integer, Producto>) ois.readObject();
+            } catch (FileNotFoundException x) {
+                System.err.format("FileNotFoundException: %s%n", x);
+            } catch (IOException x) {
+                System.err.format("IOException: %s%n", x);
+            } catch (ClassNotFoundException x) {
+                System.err.format("ClassNotFoundException: %s%n", x);
+            }
+        }
+    }
+    public void save() {
+        try (FileOutputStream fos = new FileOutputStream("database.txt");
+             ObjectOutputStream oos = new ObjectOutputStream(fos)){
+            oos.writeObject(stock);
+        } catch (FileNotFoundException x) {
+            System.err.format("FileNotFoundException: %s%n", x);
+        } catch (IOException x){
+            System.err.format("IOException: %s%n", x);
+        }
+    }
 
 }
