910512ffaa358387c914f83915fb21ca128c581a
Lsans80 <luisa.sans@gmail.com>
2023-12-08 19:08:32 +0100

    Update resources

--- src/main/java/org/example/n2Exe1MySQL/cliente/Main.java
+++ src/main/java/org/example/n2Exe1MySQL/cliente/Main.java
@@ -1,10 +1,5 @@
 package org.example.n2Exe1MySQL.cliente;
 
-import org.example.n2Exe1MySQL.entidad.Producto;
-import org.example.n2Exe1MySQL.persistencia.MySQLDB;
-
-import java.util.HashMap;
-
 public class Main {
 
     public static void main (String[] args){
@@ -12,9 +7,9 @@ public class Main {
         AplicacionFloristeria.start();
         //AplicacionFloristeria.finalizar();
 
-        MySQLDB baseDeDatos = MySQLDB.instanciar();
+        /*MySQLDB baseDeDatos = MySQLDB.instanciar();
         Producto producto = baseDeDatos.leerProducto(1);
 
-        System.out.println(producto);
+        System.out.println(producto);*/
     }
 }
